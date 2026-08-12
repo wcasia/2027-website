68493922ea7fbd4d39fb699fd2f2eb7951570fbf
Tony S <sanjayatony@gmail.com>
2026-08-12 11:06:10 +0700

    add function to go with min-width

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,18 @@
+function mediaRangeToMinWidth(root) {
+  root.walkAtRules('media', (rule) => {
+    rule.params = rule.params
+      .replace(/\(\s*width\s*>=\s*([^)]+)\)/g, '(min-width: $1)')
+      .replace(/\(\s*width\s*<=\s*([^)]+)\)/g, '(max-width: $1)')
+      .replace(/\(\s*width\s*>\s*([^)]+)\)/g, '(min-width: calc($1 + 1px))')
+      .replace(/\(\s*width\s*<\s*([^)]+)\)/g, '(max-width: calc($1 - 1px))');
+  });
+}
+
 module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    'postcss-nesting': {},
-    'postcss-custom-properties': {
-      preserve: false
-    }
-  }
+  plugins: [
+    require('@tailwindcss/postcss')(),
+    mediaRangeToMinWidth,
+    require('postcss-nesting')(),
+    require('postcss-custom-properties')({ preserve: false })
+  ]
 }
